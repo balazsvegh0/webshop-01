c75612d2d01f2bf6fb12eb91aa389921790c811f
Balázs Végh <balazsvegh0@gmail.com>
2024-02-24 02:11:30 +0100

    init

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyDBs250_7SPK2_ZwZ_VmlsRf43Bl3Y7PCk',
+    authDomain: 'webshop-61ea0.firebaseapp.com',
+    projectId: 'webshop-61ea0',
+    storageBucket: 'webshop-61ea0.appspot.com',
+    messagingSenderId: '88329526774',
+    appId: '1:88329526774:web:0cb4a01f6bec82f38a7bd7',
+  },
+  production: false,
+};
